b205a5ccbe6f35db14a5148c9c91b92b864a05fc
Manpreet Singh <manpreetsingh586@gmail.com>
2020-12-09 20:08:00 -0500

    added networking services to main.tf

--- terraform_app/dev/main.tf
+++ terraform_app/dev/main.tf
@@ -1,14 +1,7 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIARDMMB3BIITJGXR4R"
-  secret_key = "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"
 }
 
-module "my_vpc" {
+module "dev_vpc" {
     source = "../modules/vpc"
-    vpc_cidr = "192.168.0.0/16"
-    tenancy = "default"
-    vpc_id = "${module.my_vpc.vpc_id}"
-    subnet_cidr = "192.168.1.0/24"
-  
 }
